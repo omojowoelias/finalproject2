9ed29c5dcfadad928af6879181d1ab03e192422a
Elias Obed <obedelias01@gmail.com>
2023-02-02 05:57:10 -0800

    files updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';                    //react is not defined (import it)
+import  Form from "./app_component/form.component";                                             // API_ key is not defined 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import"weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./app_component/weather";
+
+//make api.openweathermap.org/data/2.5/weather?q=London,uk
+
+const API_key = "fa9ddf8d90d35a19af856add2bb374ca";
+
+//using a class component to use state.The component has to include the extends React.Component statement, this statement 
+//creates an inheritance to React.Component, and gives your component access to React.Component's functions.
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
+    //this.getWeather(); remove this method & passing e.event.target
+    this.weatherIcon={
+      Thunderstorm:"wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
+    };
+  }
+
+  calCelsius(temp){
+    let cell = Math.floor(temp - 273.15)
+    return cell;
+  }
+  
+  get_WeatherIcon(icons, rangeId){
+    switch(true){
+      case rangeId >= 200 && rangeId <=232:
+        this.setState({icon:this.weatherIcon.Thunderstorm});
+        break;
+      case rangeId >= 300 && rangeId <=321:
+        this.setState({icon:this.weatherIcon.Drizzle});
+        break;
+      case rangeId >= 500 && rangeId <=531:
+        this.setState({icon:this.weatherIcon.Rain});
+        break;
+      case rangeId >= 600 && rangeId <=622:
+        this.setState({icon:this.weatherIcon.Snow});
+        break;
+      case rangeId >= 701 && rangeId <=781:
+        this.setState({icon:this.weatherIcon.Atmosphere});
+        break;
+      case rangeId === 800:
+        this.setState({icon:this.weatherIcon.Clear});
+        break;
+      case rangeId >= 801 && rangeId <=804:
+        this.setState({icon:this.weatherIcon.Clouds});
+        break;
+        default:
+          this.setState({icon:this.weatherIcon.Clouds})
+    }
+  }
+  
+  getWeather = async (e) =>{
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    if(city && country){
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+  
+      const response = await api_call.json(); //converts the data to json format.
+      console.log(response)
+  
+      this.setState({
+        city:`${response.name},${response.sys.country}`,
+        //country:response.sys.country,
+        //celsius:response.main.temp
+        celsius:this.calCelsius(response.main.temp),
+        temp_max:this.calCelsius(response.main.temp_max),
+        temp_min:this.calCelsius(response.main.temp_min),
+        description:response.weather[0].description,
+    
+      });
+      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+    }else{
+      this.setState({error: true});
+    }
+  
+  }
+
+  render(){
+    return(
+      <div className="App">
+      <Form loadweather={this.getWeather} error={this.state.error} />
+      <Weather city={this.state.city} 
+      country={this.state.country}
+      temp_celsius={this.state.celsius}
+      temp_max={this.state.temp_max}
+      temp_min={this.state.temp_min}
+      description={this.state.description} 
+      weatherIcon={this.state.icon}
+      />
+    </div>   
+    )
+  }
 }
+
+// function App() {
+//   return (
+//   );
+// }
 
 export default App;
